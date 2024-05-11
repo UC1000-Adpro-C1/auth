2ed6fe957d7366f84fee51580259a2c57e1caf03
KenishaJazlyn <kenishajazlyn@gmail.com>
2024-05-12 05:27:30 +0700

    [GREEN] Implement Token Repository

---
+++ src/main/java/farrel/auth/repository/TokenRepository.java
@@ -0,0 +1,12 @@
+package farrel.auth.repository;
+
+import farrel.auth.model.Token;
+import farrel.auth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+    Optional<Token> findByUser(User user);
+    Optional<Token> findByToken(String token);
+}
