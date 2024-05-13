3013d955dc67d9e50d01a6257f310a0a577d4a8a
KenishaJazlyn <kenishajazlyn@gmail.com>
2024-05-14 01:59:20 +0700

    [RED] Create Test for auth controller

---
+++ src/test/java/farrel/auth/controller/AuthControllerTest.java
@@ -0,0 +1,47 @@
+package farrel.auth.controller;
+
+import farrel.auth.model.AuthResponse;
+import farrel.auth.model.User;
+import farrel.auth.service.AuthService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthControllerTest {
+
+    @InjectMocks
+    private AuthController authController;
+
+    @Mock
+    private AuthService authService;
+
+
+    @Test
+    public void testRegister() {
+        User user = new User();
+        AuthResponse expectedResponse = new AuthResponse("testToken", "User registered successfully");
+        when(authService.register(user)).thenReturn(ResponseEntity.ok(expectedResponse));
+
+        ResponseEntity<AuthResponse> response = authController.register(user);
+
+        assertEquals(expectedResponse, response.getBody());
+    }
+
+    @Test
+    public void testLogin() {
+        User user = new User();
+        AuthResponse expectedResponse = new AuthResponse("testToken", "User authenticated successfully");
+        when(authService.authenticate(user)).thenReturn(ResponseEntity.ok(expectedResponse));
+
+        ResponseEntity<AuthResponse> response = authController.login(user);
+
+        assertEquals(expectedResponse, response.getBody());
+    }
+}
